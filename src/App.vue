ac8e7f9b14ca17b6b1013bf71814b1edf53ac162
m8itsnotb8 <m8itsnotb8g@gmail.com>
2025-05-13 05:05:57 +0500

    fadsfdas

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { useI18n } from 'vue-i18n'
-import { ref } from 'vue'
-import { apiService } from '@/services/api'
-import { useRouter } from 'vue-router'
 import HeaderLinks from '@/components/HeaderLinks.vue'
 
-const router = useRouter()
-const { t, locale } = useI18n()
-
-const toggleLocale = () => {
-  const newLocale = locale.value === 'en' ? 'ru' : 'en'
-  locale.value = newLocale
-  localStorage.setItem('locale', newLocale)
-  location.reload()
-}
-
-const accountData = ref(JSON.parse(localStorage.getItem('account')))
-const isAuthorized = ref(!!localStorage.getItem('account'))
-
-async function handleLogout() {
-  try {
-    await apiService.logout()
-    router.push('/login')
-  } catch (err) {
-    console.error('Error logging out:', err)
-  }
-}
+const { t } = useI18n()
 </script>
 
 <template>
@@ -36,7 +13,6 @@ async function handleLogout() {
       <nav class="header__nav">
         <RouterLink to="/" class="header__nav-link">{{ t('nav.home') }}</RouterLink>
         <RouterLink to="/shop" class="header__nav-link">{{ t('nav.shop') }}</RouterLink>
-        <RouterLink to="/about" class="header__nav-link">{{ t('nav.guide') }}</RouterLink>
       </nav>
       <HeaderLinks />
     </div>
